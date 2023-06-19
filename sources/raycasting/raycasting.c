425ca5a322df5ed8de4951b9c257716c8c120bb0
Romain <romain.vennet@outlook.com>
2023-06-19 16:16:34 +0200

    help me god

--- sources/raycasting/raycasting.c
+++ sources/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:37:16 by romvan-d          #+#    #+#             */
-/*   Updated: 2023/06/19 15:08:36 by romvan-d         ###   ########.fr       */
+/*   Updated: 2023/06/19 15:48:08 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,6 @@ void	perform_DDA_algorithm(t_ray *ray, t_player *player, t_wall *wall, t_map *ma
 		}
 		if (map->map[player->current_square_x][player->current_square_y] == '1')
 		{
-			printf("ding");
 			wall->is_hit = true;
 		}
 	}
@@ -97,7 +96,7 @@ void	raycasting(t_cube *cube)
 	x_coord = 0;
 	while (x_coord < SCREEN_WIDTH)
 	{
-		printf("x is %d\n", x_coord);
+		//change camera depending on NSEW -> modifying dirX and planeY together or dirY and planeX together
 		instantiate_ray(&cube->camera, &cube->ray, &cube->player, &x_coord, &cube->map);
 		calculate_length_to_next_x(&cube->ray);
 		calculate_step_and_side_dist(&cube->player, &cube->ray, &cube->map);
