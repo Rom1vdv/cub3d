f6e5975d574227799fe5f32c44d3cda0c4df35c8
Romain <romain.vennet@outlook.com>
2023-09-04 13:36:56 +0200

    fixed stripe

--- sources/raycasting/raycasting2.c
+++ sources/raycasting/raycasting2.c
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:49:16 by romvan-d          #+#    #+#             */
-/*   Updated: 2023/08/31 14:37:19 by romvan-d         ###   ########.fr       */
+/*   Updated: 2023/09/04 12:17:58 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,9 +57,9 @@ void	draw_columns(t_ray *ray, t_textures *textures, t_wall *wall, t_map *map, t_
 		textures->wall_x = map->player_x + wall->shortest_dist_to_wall * ray->direction_x;
 	textures->wall_x -= floor(textures->wall_x);
 	textures->x = (int)(textures->wall_x * textures->width);
-	if (wall->which_side_hit == NORTH_SOUTH && ray->direction_x > 0)
+	if (wall->which_side_hit == NORTH_SOUTH && ray->direction_y > 0)
 		textures->x = textures->width - textures->x - 1;
-	if (wall->which_side_hit == EAST_WEST && ray->direction_y < 0)
+	if (wall->which_side_hit == EAST_WEST && ray->direction_x < 0)
 		textures->x = textures->width - textures->x - 1;
 	y += fill_wall_texture(draw, ray, textures, wall, y, mlx, x);
 	fill_floor_and_ceiling(y, draw, x, map, mlx);
--- sources/rendering/textures.c
+++ sources/rendering/textures.c
@@ -6,26 +6,11 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 14:27:23 by romvan-d          #+#    #+#             */
-/*   Updated: 2023/08/31 14:30:56 by romvan-d         ###   ########.fr       */
+/*   Updated: 2023/09/04 13:35:23 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-void	color_floor_ceiling(t_draw *draw, t_map *map, t_mlx *mlx, int y, int *x)
-{
-	if (draw->end_pos < 0)
-	{
-		draw->end_pos = SCREEN_HEIGHT;
-	}
-	y = draw->end_pos;
-	while (y < SCREEN_HEIGHT)
-	{
-		my_mlx_put_pixel(mlx, *x, y, map->floor);
-		my_mlx_put_pixel(mlx, *x, SCREEN_HEIGHT - y - 1, map->ceiling);
-		++y;
-	}
-}
 
 int	fill_wall_texture(t_draw *draw, t_ray *ray, t_textures *textures, t_wall *wall, int y, t_mlx *mlx, int *x_coord)
 {
